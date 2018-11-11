13be7e34678e1ca7449de435760f822182c55c75
Alvin Qiu <qptkk@163.com>
2018-11-11 10:12:04 +0800

    [chore]: extract parameter

--- src/main/kotlin/com/fireflysource/oj/answer/Pack01.kt
+++ src/main/kotlin/com/fireflysource/oj/answer/Pack01.kt
@@ -3,20 +3,20 @@ package com.fireflysource.oj.answer
 /**
  * @author Pengtao Qiu
  */
-const val max = 188
-val goods = intArrayOf(50, 42, 9, 15, 105, 63, 14, 30)
+fun select(goods: IntArray, max: Int): Int {
+    fun selectGood(i: Int, remain: Int): Int {
+        if (i == 0) return if (remain > goods[i]) goods[i] else 0
 
-fun make(i: Int, remain: Int): Int {
-    if (i == 0) return if (remain > goods[i]) goods[i] else 0
+        val put = if (remain > goods[i]) {
+            selectGood(i - 1, remain - goods[i]) + goods[i]
+        } else 0
 
-    val putGood = if (remain > goods[i]) {
-        make(i - 1, remain - goods[i]) + goods[i]
-    } else 0
-
-    val notPutGood = make(i - 1, remain)
-    return if (putGood > notPutGood) putGood else notPutGood
+        val notPut = selectGood(i - 1, remain)
+        return if (put > notPut) put else notPut
+    }
+    return selectGood(goods.lastIndex, max)
 }
 
 fun main(args: Array<String>) {
-    println(make(goods.lastIndex, max))
+    println(select(intArrayOf(50, 42, 9, 15, 105, 63, 14, 30), 188))
 }
