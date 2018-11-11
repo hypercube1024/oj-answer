2f4fa7399f43d650e223bc4a9b37b9307c7fba3e
Alvin Qiu <qptkk@163.com>
2018-11-11 10:07:26 +0800

    [chore]: extract parameter

--- src/main/kotlin/com/fireflysource/oj/answer/Stock.kt
+++ src/main/kotlin/com/fireflysource/oj/answer/Stock.kt
@@ -3,19 +3,18 @@ package com.fireflysource.oj.answer
 /**
  * 122. Best Time to Buy and Sell Stock
  */
-fun maxProfit(prices: IntArray): Int {
-    val n = 2
-    val buy = Array(n) { Integer.MIN_VALUE }
-    val sell = Array(n) { 0 }
+fun maxProfit(prices: IntArray, times: Int): Int {
+    val buy = Array(times) { Integer.MIN_VALUE }
+    val sell = Array(times) { 0 }
 
     for (p in prices) {
         buy[0] = Math.max(buy[0], -p)
         sell[0] = Math.max(sell[0], buy[0] + p)
 
-        for (i in 1..(n - 1)) {
+        for (i in 1..(times - 1)) {
             buy[i] = Math.max(buy[i], sell[i - 1] - p)
             sell[i] = Math.max(sell[i], buy[i] + p)
         }
     }
-    return sell[n - 1]
+    return sell[times - 1]
 }
